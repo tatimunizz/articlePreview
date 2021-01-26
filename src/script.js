9d57518fb3f82ebd5e263d33e7a7633e99c12398
Tatiana Muniz <tati.muniz0@gmail.com>
2021-01-26 23:09:44 +0300

    cleanup code to find out bug
    I cleaned up the code. I was not able to find and fix the bug of the dialog height property. The height does not compute.

--- src/script.js
+++ src/script.js
@@ -6,10 +6,14 @@ button.addEventListener("click", () =>{
     dialogBox.classList.remove('shown-dialog-box');
     dialogBox.classList.remove('dialog-box-animation');
     button.classList.remove('button-animation-in');
+    button.setAttribute("aria-expanded", "false");
+    button.setAttribute("aria-label", "Share");
     }else {
         dialogBox.classList.add('shown-dialog-box');
         dialogBox.classList.add('dialog-box-animation');
         button.classList.add('button-animation-in');
+        button.setAttribute("aria-expanded", "true");
+        button.setAttribute("aria-label", "Close share options");
     }
 
 });
